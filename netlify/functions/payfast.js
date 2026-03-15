76a369d8e19c338134ab34191f196b0c21ae78fc
sunsh1n37 <buyicube01@gmail.com>
2026-03-15 18:44:19 +0200

    Update payfast.js

--- netlify/functions/payfast.js
+++ netlify/functions/payfast.js
@@ -102,15 +102,13 @@ exports.handler = async function(event) {
 
         // ---------------------------
         // Redflag Packs R20 each
-        // ---------------------------
         case "ghosts_polterguys": amount = 20; item_name = "Redflag Pack: Ghosts & Polterguys"; break;
         case "gaslight_central": amount = 20; item_name = "Redflag Pack: Gaslight Central"; break;
         case "exes_flexes": amount = 20; item_name = "Redflag Pack: Exes and Flexes"; break;
         case "overzealous": amount = 20; item_name = "Redflag Pack: Over Zealous Over Jealous"; break;
 
         // ---------------------------
-        // RedFlag Bingo Tiles R49
-        // ---------------------------
+        // RedFlag Bingo Tiles R49 each
         case "situationships_ghosts": amount = 49; item_name = "RedFlag Bingo Tile: Situationships & Ghosts"; break;
         case "delulu_diaries": amount = 49; item_name = "RedFlag Bingo Tile: Delulu Diaries"; break;
 
